0928a9519262716bde40282206a7754ea7c559c0
Ben Stolovitz <ben@stolovitz.com>
2024-12-29 19:23:52 -0500

    yay

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,10 @@ console.log("Hi!");
 type PatchType = "aroma" | "haxchi" | "browser" | "dnspresso" | "udpih";
 const patchType: PatchType = "aroma";
 
+function getProxyUrl(url: string) {
+    return `https://corsproxy.io/?url=${url}`;
+}
+
 async function generate() {
     // https://gbatemp.net/threads/how-to-set-up-isfshax.642258/
     console.log("Downloading files...");
@@ -12,33 +16,32 @@ async function generate() {
     // Flipped for "latest" tag
     // https://stackoverflow.com/questions/24987542/is-there-a-link-to-github-for-downloading-a-file-in-the-latest-release-of-a-repo
     // https://github.com/isfshax/isfshax_installer/releases/download/v2.0/ios.img
-    const ios_req = await fetch("https://corsproxy.io/?url=https://github.com/isfshax/isfshax_installer/releases/latest/download/ios.img")
+    const ios_req = await fetch(getProxyUrl("https://github.com/isfshax/isfshax_installer/releases/latest/download/ios.img"));
 
     // https://github.com/isfshax/isfshax/releases/download/v4.0/superblock.img
     // https://github.com/isfshax/isfshax/releases/download/v4.0/superblock.img.sha
-    const superblock_req = await fetch("https://corsproxy.io/?url=https://github.com/isfshax/isfshax/releases/latest/download/superblock.img");
-    const superblock_sha_req = await fetch("https://corsproxy.io/?url=https://github.com/isfshax/isfshax/releases/latest/download/superblock.img.sha");
+    const superblock_req = await fetch(getProxyUrl("https://github.com/isfshax/isfshax/releases/latest/download/superblock.img"));
+    const superblock_sha_req = await fetch(getProxyUrl("https://github.com/isfshax/isfshax/releases/latest/download/superblock.img.sha"));
 
     // https://github.com/jan-hofmeier/stroopwafel/releases/download/redseeprom-v1.1/wafel_core.ipx
-    const wafel_core_req = await fetch("https://corsproxy.io/?url=https://github.com/jan-hofmeier/stroopwafel/releases/latest/download/wafel_core.ipx");
+    const wafel_core_req = await fetch(getProxyUrl("https://github.com/jan-hofmeier/stroopwafel/releases/latest/download/wafel_core.ipx"));
 
     // https://github.com/isfshax/wafel_isfshax_patch/releases/download/v3.1.1/wafel_isfshax_patch.ipx
-    const wafel_isfshax_patch_req = await fetch("https://corsproxy.io/?url=https://github.com/isfshax/wafel_isfshax_patch/releases/latest/download/wafel_isfshax_patch.ipx");
+    const wafel_isfshax_patch_req = await fetch(getProxyUrl("https://github.com/isfshax/wafel_isfshax_patch/releases/latest/download/wafel_isfshax_patch.ipx"));
 
     // For non-UDPIH
     let fw_encrypted_req: Response | undefined = undefined;
     if (patchType !== "udpih") {
         // https://github.com/jan-hofmeier/minute_minute/releases/download/redseeprom-v1.0/fw_encrypted.img
         // https://github.com/wiiu-env/fw_img_payload/releases/download/v0.1/fw_img_payload_20201201-192754.zip
-        fw_encrypted_req = await fetch("https://corsproxy.io/?url=https://github.com/jan-hofmeier/minute_minute/releases/latest/download/fw_encrypted.img");
+        fw_encrypted_req = await fetch(getProxyUrl("https://github.com/jan-hofmeier/minute_minute/releases/latest/download/fw_encrypted.img"));
     }
     
     let fw_img_payload_req: Response | undefined = undefined;
     if (patchType === "browser" || patchType === "dnspresso") {
-        fw_img_payload_req = await fetch("https://corsproxy.io/?url=https://github.com/wiiu-env/fw_img_payload/releases/latest/download/fw_img_payload_20201201-192754.zip");
+        fw_img_payload_req = await fetch(getProxyUrl("https://github.com/wiiu-env/fw_img_payload/releases/latest/download/fw_img_payload_20201201-192754.zip"));
     }
 
-    // For UDPIH
     let fw_img_req: Response | undefined = undefined;
     let boot1_req: Response | undefined = undefined;
     let recovery_menu_req: Response | undefined = undefined;
@@ -46,9 +49,9 @@ async function generate() {
         // https://github.com/jan-hofmeier/minute_minute/releases/download/redseeprom-v1.0/fw.img    
         // https://github.com/jan-hofmeier/minute_minute/releases/download/redseeprom-v1.0/boot1.img
         // https://github.com/GaryOderNichts/recovery_menu/releases/download/v0.6/recovery_menu
-        fw_img_req = await fetch("https://corsproxy.io/?url=https://github.com/jan-hofmeier/minute_minute/releases/latest/download/fw.img");
-        boot1_req = await fetch("https://corsproxy.io/?url=https://github.com/jan-hofmeier/minute_minute/releases/latest/download/boot1.img");
-        recovery_menu_req = await fetch("https://corsproxy.io/?url=https://github.com/GaryOderNichts/recovery_menu/releases/latest/download/recovery_menu");
+        fw_img_req = await fetch(getProxyUrl("https://github.com/jan-hofmeier/minute_minute/releases/latest/download/fw.img"));
+        boot1_req = await fetch(getProxyUrl("https://github.com/jan-hofmeier/minute_minute/releases/latest/download/boot1.img"));
+        recovery_menu_req = await fetch(getProxyUrl("https://github.com/GaryOderNichts/recovery_menu/releases/latest/download/recovery_menu"));
     }
 
     // fw_img_payload_req is a zip file with a folder named `wiiu` containing a `payload.elf`; get that file.
